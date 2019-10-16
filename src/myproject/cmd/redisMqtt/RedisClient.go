19fe12b6f96c7cdce20d0f9612490bca0ec4d92a
Xabi <ale23@hotmail.fr>
2019-10-16 16:43:53 +0200

    Modify RedisClient to add et receive RedisEntry in redis db

--- src/myproject/cmd/redisMqtt/RedisClient.go
+++ src/myproject/cmd/redisMqtt/RedisClient.go
@@ -4,49 +4,98 @@ import (
 	"MomenGo/src/myproject/internal/entities"
 	"github.com/gomodule/redigo/redis"
 	"log"
+	"strings"
 )
 
 type RedisClient struct {
-	network string
-	address string
+	config 	entities.RedisDB
 	conn    redis.Conn
 }
 
 func CreateARedisClient(network string, address string) RedisClient {
-	return RedisClient{network: network, address: address, conn: nil}
+	return RedisClient{config: entities.RedisDB{Network: network, Address: address}, conn: nil}
+}
+func CreateARedisClientFromConfig(config entities.RedisDB) RedisClient {
+	return RedisClient{config: config, conn: nil}
 }
 
-func (c *RedisClient) connectionToServer() redis.Conn {
+func (r *RedisClient) connectionToServer() redis.Conn {
 	// Connect to the Redis server (default port is 6379)
 	// conn, err := redis.Dial("tcp", "localhost:6379")
 	var err error
-	c.conn, err = redis.Dial(c.network, c.address)
+	r.conn, err = redis.Dial(r.config.Network, r.config.Address)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Ensure the connection is always closed before exiting
 	//defer c.conn.Close()
-	return c.conn
+	return r.conn
 }
-
-func (c *RedisClient) CreateAnEntry(entry *entities.RedisEntry) *redis.Conn {
-	c.connectionToServer()
-	_, err := c.connectionToServer().Do("RPUSH", "captor", entry.RedisEntryToString())
+func (r *RedisClient) doesKeysExists (tabKeys []string) bool{
+	res, err := redis.Bool(
+		r.connectionToServer().Do("EXISTS", strings.Join(tabKeys, " ")))
 	if err != nil {
 		log.Fatal(err)
 	}
-	//defer c.conn.Close()
-	defer c.conn.Close()
-	return &c.conn
+	return res
 }
 
-func (c *RedisClient) GetAnEntry(entry string) string {
-	r, err := redis.String(c.conn.Do("GET", entry))
+func (r *RedisClient) AddCaptorEntryToDB (entry *entities.RedisEntry)  {
+	key := []string{entry.CaptorKey()}
+	values := entry.GetCaptorValues()
+	r.connectionToServer()
+	defer r.conn.Close()
+	// Ensure there is already a table for this Captor and this CaptorValues
+	if r.doesKeysExists(key) {
+		for i := 0 ; i < len(values) ; i++ {
+			_, err := r.connectionToServer().Do("RPUSH", entry.CaptorValuesKey(), values[i])
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	} else {
+		_, err := r.connectionToServer().Do("HMSET", entry.CaptorKey(),
+			"idAirport", entry.Captor.GetIdAirportToString(),
+			"idCaptor", entry.Captor.GetIdCaptorToString(),
+			"measure", entry.Captor.GetMeasureToString(),
+			"day", "lol",
+			"values", entry.CaptorValuesKey())
+		if err != nil {
+			log.Fatal(err)
+		}
+		r.AddCaptorEntryToDB(entry)
+	}
+}
+//func (r *RedisClient) CreateAnEntry(entry *entities.RedisEntry) *redis.Conn {
+//	r.connectionToServer()
+//	// Ensure there is already an entry for this Captor
+//	_, err := r.connectionToServer().Do("RPUSH", "captor", entry.RedisEntryToString())
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	//defer r.conn.Close()
+//	defer r.conn.Close()
+//	return &r.conn
+//}
+//
+func (r *RedisClient) GetACaptorAttributeEntry(key string, value string) string {
+	r.connectionToServer()
+	res, err := redis.Strings(r.conn.Do("HMGET", key, value))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer c.conn.Close()
-	return r
+	defer r.conn.Close()
+	return res[0]
+}
+
+func (r *RedisClient) GetAllCaptorValuesEntry(key string) []string {
+	r.connectionToServer()
+	res, err := redis.Strings(r.conn.Do("LRANGE", key, "0","-1"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.conn.Close()
+	return res
 }
 
 func main() {
