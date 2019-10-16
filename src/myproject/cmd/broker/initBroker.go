6b300666e0d189c3da1e8d9e45066af25badaac4
Ismail EL KHANTACHE <ismail.elkhantache@amplexor.com>
2019-10-16 17:25:04 +0200

    Fix import path

---
+++ src/myproject/cmd/broker/initBroker.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"log"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	//TODO: Check if you can start a mqtt broker with paho
+	wg.Add(1)
+
+	opts := mqtt.NewClientOptions().AddBroker("tcp://localhost:1883")
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		log.Fatal(token.Error())
+	}
+
+	wg.Wait()
+
+}
