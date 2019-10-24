db975d8a11bac583b4337358d5b9a110e7cdff65
Xabi <ale23@hotmail.fr>
2019-10-24 17:37:16 +0200

    Completed the RedisClientSample

--- src/myproject/cmd/sample/RedisClientSample/RedisClientSample.go
+++ src/myproject/cmd/sample/RedisClientSample/RedisClientSample.go
@@ -1,8 +1,10 @@
-package RedisClientSample
+package main
 
 import (
+	"fmt"
 	"myproject/cmd/redisMqtt"
 	"myproject/internal/entities"
+	"time"
 )
 
 func main() {
@@ -14,15 +16,15 @@ func main() {
 		`{
 			"idCaptor":1,
 			"idAirport":"AAI",
-			"measure":"TEST",
+			"measure":"TEMP",
 			"values":[
 				{
 					"value":27.8,
-					"timestamp":"2007-03-01T13:00:00Z"
+					"timestamp":"2007-03-01T13:05:05Z"
 				},
 				{
 					"value":32.1,
-					"timestamp":"2008-03-01T13:00:00Z"
+					"timestamp":"2008-03-01T13:05:05Z"
 				}
 			]
 		}`)
@@ -34,8 +36,6 @@ func main() {
 	println("This is the RedisEntry produced :\t", r.RedisEntryToString())
 	println("This is the key of the Captor hash")
 	println(r.CaptorKey())
-	println("This is the key of the CaptorValues list")
-	println(r.CaptorValuesKey())
 
 	// Create a RedisClient with the config
 	rc := redisMqtt.CreateARedisClientFromConfig(config)
@@ -43,12 +43,23 @@ func main() {
 	// Add the RedisEntry to the redis DB
 	rc.AddCaptorEntryToDB(r)
 
-	// Get the entry of the captor created
-	println(rc.GetACaptorAttributeEntry(r.CaptorKey(), "idAirport"))
+	// Get the keys corresponding to the interval between 01-01-2007 and 01-03-2008
+	keys, _ := rc.GetCaptorValuesKeysInInterval(
+		[]string{r.CaptorKey()},
+		time.Date(2007,01,01,0,0,0,0,time.UTC),
+		time.Date(2008,03,01,0,0,0,0,time.UTC))
+	fmt.Println("Keys between 2000 and 2019",keys)
 
-	// Get the entry of the captor created
-	v := rc.GetAllCaptorValuesEntry(r.CaptorValuesKey())
-	for i := 0; i < len(v); i++ {
-		println(v[i])
+	// Get the values correspond to the interval chosen before
+	var res []string
+	for i := 0; i < len(keys); i++ {
+		val, _ := rc.GetAllCaptorValuesEntries(keys[i])
+		for j := 0 ; j < len(val) ; j++  {
+			res = append(res,val[j])
+		}
+	}
+	fmt.Println("This are the values stored in the db :")
+	for i := 0; i < len(res) ; i++  {
+		fmt.Println(res[i])
 	}
 }
