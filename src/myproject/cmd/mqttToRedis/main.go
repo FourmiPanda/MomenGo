aad41352fdb9d8090c53ac78d25c5e8d4f224f29
Xabi <ale23@hotmail.fr>
2019-10-27 10:07:01 +0100

    Add a Mqtt to Redis application

---
+++ src/myproject/cmd/mqttToRedis/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"myproject/cmd/redisMqtt"
+	"myproject/internal/entities"
+)
+
+func main() {
+	c := entities.GetConfig()
+	m := redisMqtt.CreateAMqttClientFromConfig(&c)
+
+	// Le client MQTT s'abonne à tous les topics contenu dnas capteurs
+	m.SubscribeAToATopic("/capteurs/#")
+
+	for {
+		continue
+	}
+}
